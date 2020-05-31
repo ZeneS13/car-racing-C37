b1fb113048db6eed51a8305be86a4970db82d917
ZeneS13 <sangoi.zene@gmail.com>
2020-05-31 10:54:58 +0530

    modified

--- sketch.js
+++ sketch.js
@@ -7,10 +7,11 @@ var distance = 0;
 var database;
 
 var form, player, game;
+var car, car2,car3,car4,cars;
 
 
 function setup(){
-  canvas = createCanvas(400,400);
+  canvas = createCanvas(displayWidth,displayHeight);
   database = firebase.database();
   game = new Game();
   game.getState();
@@ -26,4 +27,5 @@ function draw(){
     clear();
     game.play();
   }
+  
 }
